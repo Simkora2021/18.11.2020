7b81a3e6409ad489a3bf6fa673af42f51dc2d333
Simkora <simkora1996@gmail.com>
2020-11-18 14:25:53 +0300

    finish calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+        public double self() {
+            Scanner sc = new Scanner(System.in);
+            double i = 0;
+            if (sc.hasNextDouble()) {
+                i = sc.nextDouble();
+            } else {
+                System.out.println(" neverno vvedeno ");
+            }
+            return i;
+        }
+    }
